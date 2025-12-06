adb64be80fd637a3fe3034de30dc2b7864cb6bdd
IsabellyDiasLimaDev <isabelly.developer@gmail.com>
2025-12-06 13:55:10 -0300

    chore: inicializa projeto spring boot

---
+++ src/main/java/br/com/magalu/sbootapiwishlist/SbootApiWishlistApplication.java
@@ -0,0 +1,13 @@
+package br.com.magalu.sbootapiwishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootApiWishlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbootApiWishlistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/magalu/sbootapiwishlist/SbootApiWishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.magalu.sbootapiwishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootApiWishlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
